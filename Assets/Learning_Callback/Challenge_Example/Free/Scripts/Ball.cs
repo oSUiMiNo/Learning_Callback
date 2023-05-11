d17ff4cf8fc5adbd22503362b04c9315ada61e06
大平 周生 <t170d025@gunma-u.ac.jp>
2023-05-11 22:56:51 +0900

    初回

---
+++ Assets/Learning_Callback/Challenge_Example/Free/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float speed = 15;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+        GameManager.OnInit += () => transform.position = new Vector3(0, 1.4f, -6.5f);
+        GameManager.OnBlocksLost += () => rb.velocity = Vector3.zero;
+        
+        InputCallbacks.OnKeyDown_Space += () =>
+        {
+            if (GameManager.gameSet || !GameManager.Initialized) return;
+            rb.velocity = new Vector3(speed, 0, speed);
+        };
+    }
+
+
+    void Update()
+    {
+        if (GameManager.gameSet) return;
+        // 現在の速度を取得
+        Vector3 velocity = rb.velocity;
+        // (speed ～ speed * 1.5) の範囲内に補正した速度を計算
+        float clampedSpeed = Mathf.Clamp(velocity.magnitude, speed, speed * 1.5f);
+        // 速度を反映
+        rb.velocity = velocity.normalized * clampedSpeed;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // プレイヤーに当たったときに、跳ね返る方向を変える
+        if (collision.gameObject.tag == "Racket")
+        {
+            // プレイヤーの位置を取得
+            Vector3 playerPos = collision.transform.position;
+            // ボールの位置を取得
+            Vector3 ballPos = transform.position;
+            // プレイヤーから見たボールの方向を計算
+            Vector3 direction = (ballPos - playerPos).normalized;
+            // 現在の速さを取得
+            float speed = rb.velocity.magnitude;
+            // 速度を変更
+            rb.velocity = direction * speed;
+        }
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // バウンドの方向が修飾子て永久に真横にバウンドし続けることがあるので、毎バウンド時に少しだけランダムな方向に力をかける
+        rb.AddForce(new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2)) * 70);
+    }
+}
